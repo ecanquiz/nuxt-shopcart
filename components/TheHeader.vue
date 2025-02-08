01e10627b471219548da13b1882edcdb516bb27c
Ernesto Canquiz <caribestic123@gmail.com>
2025-02-08 18:33:14 -0400

    feat: Create layouts folder

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -19,10 +19,10 @@ export default {
 
 <template>
   <header
-    class="flex justify-between p-6 mb-10 items-center"
+    class="flex justify-between p-6 mb-10 items-center sticky top-0"
     style="background-image: url('/images/double-bubble-outline.png')"
   >
-    <h1 class="text-4xl text-gray-700 font-bold">The Pineapple Stand</h1>
+    <h1 class="text-4xl text-gray-700 font-bold">Bodega.com</h1>
     <div>
       <span class="mr-5" @click="toTwitter">{{ user }}</span>
       <CartWidget class="inline-block" />

b4dd3dae13f54ce4607efef15449389902f950e1
Ernesto Canquiz <caribestic123@gmail.com>
2025-04-05 19:20:22 -0400

    fix: Color-mode functionality

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,20 +1,11 @@
-<script>
+<script setup lang="ts">
+import { computed } from "vue"
 import { useAuthUserStore } from "@/stores/AuthUserStore";
-import { mapState, mapActions } from "pinia";
 import CartWidget from "./CartWidget.vue";
-export default {
-  components: { CartWidget },
-  computed: {
-    ...mapState(useAuthUserStore, {
-      user: "username",
-    }),
-  },
-  methods: {
-    ...mapActions(useAuthUserStore, {
-      toTwitter: "visitTwitterProfile",
-    }),
-  },
-};
+
+const authUserStore = useAuthUserStore();
+const user = computed(() => authUserStore.username);
+const toTwitter = () => authUserStore.visitTwitterProfile();
 </script>
 
 <template>
@@ -32,22 +23,7 @@ export default {
             type="text"
             placeholder="Search products..." 
             class="px-4 py-2 rounded-lg border border-gray-300 dark:border-gray-600 dark:bg-gray-700 dark:text-white focus:ring-2 focus:ring-primary-400 transition-all"
-          />
-                  
-          <button
-            onclick="toggleDarkMode()" 
-            class="p-2 rounded-lg bg-primary-500 text-white hover:bg-primary-600 transition-colors group"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6 group-hover:rotate-180 transition-transform"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
-            </svg>
-          </button>                    
+          />          
         </div>
 
         <div>
